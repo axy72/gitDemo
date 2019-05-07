50dc168921b3aab3a6742a7318cfa1daa7071c85
axy72 <Abraham.Sanchez@corp.nearshoretc.com>
2019-05-07 12:45:18 -0500

    final commit by USA TEAM

---
+++ src/test/java/qaclickacademy/gitDemo.java
@@ -0,0 +1,12 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class gitDemo {
+
+	
+	@Test 
+	public void postJira() {
+		System.out.println("****** ADD FILE GIT DEMO");
+	}
+}
